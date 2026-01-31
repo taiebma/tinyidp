e2cafe3530acfb2bc20bb07b592ceb82fed65e1b
Marc TAIEB <taiebma@free.fr>
2026-01-31 22:26:04 +0100

    Transformation des recherches en version compilée

--- tinyidp/Infrasctructure/Bdd/CredentialRepository.cs
+++ tinyidp/Infrasctructure/Bdd/CredentialRepository.cs
@@ -6,7 +6,19 @@ namespace tinyidp.infrastructure.bdd;
 public class CredentialRepository : ICredentialRepository
 {
     private readonly TinyidpContext _tinyidpContext;
-    
+    private static readonly Func<TinyidpContext, string, Task<Credential?>> _getByIdent =
+        EF.CompileAsyncQuery((TinyidpContext context, string ident) =>
+            context.Credentials.Where(u => u.Ident == ident).FirstOrDefault());
+        private static readonly Func<TinyidpContext, string, Task<Credential?>> _getByIdentReadonly =
+            EF.CompileAsyncQuery((TinyidpContext context, string ident) =>
+            context.Credentials.Where(u => u.Ident == ident).AsNoTracking().FirstOrDefault());
+        private static readonly Func<TinyidpContext, string, Task<Credential?>> _getByAuthorizationCode =
+            EF.CompileAsyncQuery((TinyidpContext context, string code) =>
+            context.Credentials.Where(u => u.AuthorizationCode == code).AsNoTracking().FirstOrDefault());
+        private static readonly Func<TinyidpContext, string, Task<Credential?>> _getByRefreshToken =
+            EF.CompileAsyncQuery((TinyidpContext context,  string token) =>
+            context.Credentials.Where(u => u.RefreshToken == token).AsNoTracking().FirstOrDefault());
+
     public CredentialRepository( TinyidpContext tinyidpContext)
     {
         _tinyidpContext = tinyidpContext;
@@ -38,8 +50,7 @@ public class CredentialRepository : ICredentialRepository
 
     public Task<Credential?> GetByIdent(string ident)
     {
-        return _tinyidpContext.Credentials
-            .Where<Credential>(p => p.Ident == ident).FirstOrDefaultAsync();
+        return _getByIdent(_tinyidpContext, ident);
     }
 
     public Credential? GetByIdReadOnly(int id)
@@ -60,9 +71,7 @@ public class CredentialRepository : ICredentialRepository
 
     public Task<Credential?> GetByIdentReadOnly(string ident)
     {
-        return _tinyidpContext.Credentials
-            .Where<Credential>(p => p.Ident == ident).AsNoTracking()
-            .FirstOrDefaultAsync();
+        return _getByIdentReadonly(_tinyidpContext, ident);
     }
 
     public Task<List<Credential>> SearchByIdentLike(string ident)
@@ -76,16 +85,12 @@ public class CredentialRepository : ICredentialRepository
 
     public Task<Credential?> GetByAuthorizationCode(string code)
     {
-        return _tinyidpContext.Credentials
-            .Where<Credential>(p => p.AuthorizationCode == code)
-            .AsNoTracking().FirstOrDefaultAsync();
+        return _getByAuthorizationCode(_tinyidpContext, code);
     }
 
     public Task<Credential?> GetByRefreshToken(string token)
     {
-        return _tinyidpContext.Credentials
-            .Where<Credential>(p => p.RefreshToken == token)
-            .AsNoTracking().FirstOrDefaultAsync();
+        return _getByRefreshToken(_tinyidpContext, token);
     }
 
     public Task<List<Credential>> SearchByState(int state )
